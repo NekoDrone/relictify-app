0d5923d01e41dbcb1019317c4688c850523fbe53
Sylfr Serenity Tan <98162476+NekoDrone@users.noreply.github.com>
2024-03-18 18:05:22 +0800

    feat: PageInfo component

---
+++ src/components/PageInfo/PageInfo.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { CombatPath, humanisePathEnum } from "@/exports/entities";
+import { FC } from "react";
+
+export interface PageInfoProps {
+    pageTitle: string,
+    combatPath: CombatPath
+    activeChar: string
+}
+
+const PageInfo: FC<PageInfoProps> = ({
+    pageTitle,
+    combatPath,
+    activeChar,
+}) => {
+    return (
+        <div className={"flex items-center gap-2 p-16"}>
+            <Image
+                src={"/assets/general/character_generic.webp"}
+                alt={"Character Info Icon"}
+                width={55}
+                height={55}
+                className={"gold-filter"}
+            />
+            <div>
+                <p className="text-lg text-gold font-medium">{pageTitle}</p>
+                <div className={"flex"}>
+                    <Image
+                        src={`/assets/combat_paths/path_${combatPath}.webp`}
+                        alt={"Path Icon"}
+                        width={24}
+                        height={16}
+                        className={"p-1"}
+                    />
+                    <p className="text-xl font-medium">{humanisePathEnum(combatPath)} / {activeChar}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PageInfo
