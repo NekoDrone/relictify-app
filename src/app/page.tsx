53eab27aad8614b8ddd082acaa1fe7065cff821a
Sylfr Serenity Tan <98162476+NekoDrone@users.noreply.github.com>
2024-03-18 18:07:55 +0800

    chore: conform to arrow function format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 
-import Image from "next/image";
 import Link from "next/link";
 
-export default function Landing() {
-  return <div className="flex min-h-screen flex-col items-center justify-center p-32">
-    <h1 className="text-center p-4">Welcome to Relictify</h1>
-    <Link href={"/app"}>
-      <button className="bg-amber-500 m-0 rounded p-2">TEMP: To App :)</button>
-    </Link>
-  </div>
+const LandingPage = () => {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center p-32">
+            <h1 className="text-center p-4">Welcome to Relictify</h1>
+            <Link href={"/app"}>
+                <button className="bg-amber-500 m-0 rounded p-2">TEMP: To App :)</button>
+            </Link>
+        </div>
+    );
 }
+
+export default LandingPage
