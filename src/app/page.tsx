7962aa56fa168b6e8148462ccada7b0c5485cc53
Sylfr Serenity Tan <98162476+NekoDrone@users.noreply.github.com>
2024-03-18 15:30:35 +0800

    chore: lint error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Landing() {
   return <div className="flex min-h-screen flex-col items-center justify-center p-32">
     <h1 className="text-center p-4">Welcome to Relictify</h1>
-    <Link href="/app">
+    <Link href={"/app"}>
       <button className="bg-amber-500 m-0 rounded p-2">TEMP: To App :)</button>
     </Link>
   </div>
