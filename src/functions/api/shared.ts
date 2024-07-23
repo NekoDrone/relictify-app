6db8be1202984bf256fffdb1b2379220963d0187
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-07-23 17:56:47 +0800

    feat: add assets url

--- src/functions/api/shared.ts
+++ src/functions/api/shared.ts
@@ -1,7 +1,9 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.relictify.com/';
 export const API_KEY = process.env.NEXT_PUBLIC_API_KEY || '';
 
-export enum Endpoints {
+export enum ApiEndpoints {
     GET_CHARS = "getChars",
     GET_CHAR = "getChar"
 }
+
+export const ASSETS_URL = process.env.NEXT_PUBLIC_ASSETS_URL || 'https://assets.relictify.com/get/'
