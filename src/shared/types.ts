8830000d8fbdf3e93bb745219cfcef9348094b80
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-07-23 14:02:29 +0800

    feat: add character interface

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,5 +1,19 @@
+import { CharPath, CombatElement } from "@/shared/entities";
+
 export interface ParamsProps {
     params: {
         id: string
     }
+}
+
+export interface Character {
+    id: number,
+    rarity: 4 | 5,
+    name: string
+    element: CombatElement
+    path: CharPath
+    base_hp_80: number
+    base_atk_80: number,
+    base_def_80: number,
+    base_spd: number
 }
