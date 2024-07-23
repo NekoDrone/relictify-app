92f23ba3882d8abd98bbc1d7eac4e91eeee9887f
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-07-23 13:25:48 +0800

    fix: add correct endpoint

--- src/functions/api/getManifest.ts
+++ src/functions/api/getManifest.ts
@@ -1,8 +1,8 @@
-import { API_KEY, API_URL } from "@/functions/api/shared";
+import { API_KEY, API_URL, Endpoints } from "@/functions/api/shared";
 
 const getCharactersManifest = async () => {
     const req = new Request(
-        API_URL,
+        `${API_URL}/${Endpoints.GET_CHARS}`,
         { headers: { 
             'Content-Type': 'application/json' ,
             'Authorization': `Bearer ${API_KEY}`
