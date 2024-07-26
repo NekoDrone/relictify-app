f184409a2de59bd6fc08eec0ee6684b3253a1960
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-07-27 00:04:41 +0800

    feat: add dev console log

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import "./globals.css";
-import React from "react";
+import React, { useEffect } from "react";
 import localFont from "next/font/local";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AssetsProvider } from "@/utilities/providers/AssetsProvider";
 import { ManifestProvider } from "@/utilities/providers/ManifestProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const din = localFont({
     src: "../../public/fonts/DIN.ttf",
@@ -18,12 +19,18 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    useEffect(() => {
+        if (process.env.NODE_ENV === "development")
+            console.log("Relictify running in development mode.");
+    }, []);
+
     return (
         <>
             <title>Relictify</title>
             <html lang="en">
                 <body className={din.className}>
                     <QueryClientProvider client={queryClient}>
+                        <ReactQueryDevtools />
                         <AssetsProvider>
                             <ManifestProvider>
                                 <main>{children}</main>
