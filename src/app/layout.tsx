af911351c80d991de4eeb07438e90b331dfb3d43
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-07-26 21:00:17 +0800

    chore: run prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import type { Metadata } from "next";
 import "./globals.css";
-import React, { Suspense } from "react";
+import React from "react";
 import localFont from "next/font/local";
-import { PomPomLoading } from "@/components/Loading/PomPomLoading";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AssetsProvider } from "@/utilities/providers/AssetsProvider";
 import { ManifestProvider } from "@/utilities/providers/ManifestProvider";
