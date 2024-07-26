5c775a99e6d98066189f49d9fe46395332e24a09
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-07-26 20:20:41 +0800

    feat: add manifest type

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -2,18 +2,24 @@ import { CharPath, CombatElement } from "@/shared/entities";
 
 export interface ParamsProps {
     params: {
-        id: string
-    }
+        id: string;
+    };
 }
 
 export interface Character {
-    id: number,
-    rarity: 4 | 5,
-    name: string
-    element: CombatElement
-    path: CharPath
-    base_hp_80: number
-    base_atk_80: number,
-    base_def_80: number,
-    base_spd: number
+    id: number;
+    rarity: 4 | 5;
+    name: string;
+    element: CombatElement;
+    path: CharPath;
+    base_hp_80: number;
+    base_atk_80: number;
+    base_def_80: number;
+    base_spd: number;
+}
+
+export interface Manifest {
+    manifest: number[];
+    version: number;
+    lastUpdated: Date;
 }
