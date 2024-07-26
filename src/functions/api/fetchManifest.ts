6b3e145ef828220c30978096ce1d9d812ee0ca74
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-07-26 20:19:19 +0800

    feat: fetching manifest prioritise local

--- src/functions/api/fetchManifest.ts
+++ src/functions/api/fetchManifest.ts
@@ -1,17 +1,33 @@
 import { API_KEY, API_URL, ApiEndpoints } from "@/functions/api/shared";
+import { Character } from "@/shared/types";
 
-const fetchCharactersManifest = async () => {
+const fetchManifestAndStore = async () => {
+    console.log("Characters not found in localStorage, fetching...");
+
     const req = new Request(`${API_URL}/${ApiEndpoints.GET_CHARS}`, {
         headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${API_KEY}`,
         },
     });
-    return (await fetch(req)).json();
+
+    const characters: Character[] = await (await fetch(req)).json();
+
+    localStorage.setItem("characters", JSON.stringify(characters));
+
+    return characters;
+};
+
+const fetchManifestFromStorage = () => {
+    return JSON.parse(
+        localStorage.getItem("characters") ?? "undefined",
+    ) as Character[];
 };
 
 export const fetchManifest = async () => {
+    const characters = localStorage.getItem("characters");
+    if (characters === null) await fetchManifestAndStore();
     return {
-        characters: await fetchCharactersManifest(),
+        characters: fetchManifestFromStorage(),
     };
 };
