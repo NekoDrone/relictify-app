18476134413646cf73ab0b0c0f970010f1217733
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2024-07-26 20:50:56 +0800

    feat, chore: add manifest api route, run prettier

--- src/functions/api/shared.ts
+++ src/functions/api/shared.ts
@@ -1,9 +1,12 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.relictify.com/';
-export const API_KEY = process.env.NEXT_PUBLIC_API_KEY || '';
+export const API_URL =
+    process.env.NEXT_PUBLIC_API_URL || "https://api.relictify.com/";
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY || "";
 
 export enum ApiEndpoints {
     GET_CHARS = "getChars",
-    GET_CHAR = "getChar"
+    GET_CHAR = "getChars",
+    GET_CHAR_MANIFEST = "getCharManifest",
 }
 
-export const ASSETS_URL = process.env.NEXT_PUBLIC_ASSETS_URL || 'https://assets.relictify.com/get/'
+export const ASSETS_URL =
+    process.env.NEXT_PUBLIC_ASSETS_URL || "https://assets.relictify.com/get/";
